9e51d4fe3ce17d3479edaa666f12381f5e06f453
Lex <konstantin.lex@gmail.com>
2012-07-04 23:37:06 +0300

    ticket:1 tests fail, add status_code to tests

--- testproj/testappp/tests.py
+++ testproj/testappp/tests.py
@@ -8,5 +8,6 @@ class BasicTest(TestCase):
         self.assertTrue(BioModel.objects.count()>0)
         test_bio = BioModel.objects.all()[0]
         resp = self.client.get("/")
+        self.assertEqual(resp.status_code,200)
         for fied, value in test_bio.get_fields()[1:]:
             self.assertIn(value, resp.content)
