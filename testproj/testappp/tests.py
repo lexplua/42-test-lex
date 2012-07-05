6fdad074f6114e84f08ab424ac85cce6b5ebc585
Lex <konstantin.lex@gmail.com>
2012-07-05 09:04:11 +0300

    ticket:3. Tests added

--- testproj/testappp/tests.py
+++ testproj/testappp/tests.py
@@ -2,6 +2,8 @@
 __author__ = 'lex'
 from django.test import TestCase
 from testproj.testappp.models import BioModel
+from datetime import datetime
+from random import randrange,choice
 
 class BasicTest(TestCase):
     def test_check_data(self):
@@ -11,3 +13,15 @@ class BasicTest(TestCase):
         self.assertEqual(resp.status_code,200)
         for fied, value in test_bio.get_fields()[1:]:
             self.assertIn(value, resp.content)
+
+class MiddlewareTest(TestCase):
+    def test_check_data(self):
+        data = []
+        for i in range(5):
+            now_date = datetime.now().strftime("%d/%m/%Y %H:%M")
+            resp = self.client.get(choice(("/","/requests/")))
+            data.append(now_date)
+        resp = self.client.get("/requests/")
+        self.assertEqual(resp.status_code,200)
+        for i in data:
+            self.assertIn(i, resp.content)
