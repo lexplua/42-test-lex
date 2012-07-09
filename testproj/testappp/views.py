004be11e1dace05a661c80b6fe63809713b038c1
Lex <konstantin.lex@gmail.com>
2012-07-09 23:22:30 +0300

    PEP8

--- testproj/testappp/views.py
+++ testproj/testappp/views.py
@@ -1,7 +1,8 @@
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView,ListView,UpdateView
+from django.views.generic import DetailView, ListView, UpdateView
 from django.contrib.auth.decorators import login_required
 from testproj.testappp.models import *
+
 
 class BioView(DetailView):
     model = BioModel
@@ -15,11 +16,13 @@ class RequestView(ListView):
     template_name = 'middleware_request.html'
     paginate_by = 10
 
+
 class EditBioView(UpdateView):
     model = BioModel
     template_name = 'edit_bio.html'
     success_url = '/'
     context_object_name = 'bio'
+
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        return super(EditBioView,self).dispatch(request, *args, **kwargs)
+        return super(EditBioView, self).dispatch(request, *args, **kwargs)
