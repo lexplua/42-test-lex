2b2a8ea74dca5a591d3dc4e71ef5bd31639980c8
Lex <konstantin.lex@gmail.com>
2012-07-09 16:29:28 +0300

    ticket:6.  Passed tests

--- testproj/testappp/forms.py
+++ testproj/testappp/forms.py
@@ -1,7 +1,9 @@
 __author__ = 'lex'
 from django import forms
 from testproj.testappp.models import BioModel
-class EditUserForm(forms.ModelForm):
 
+
+
+class EditUserForm(forms.ModelForm):
     class Meta:
         model = BioModel
--- testproj/testappp/views.py
+++ testproj/testappp/views.py
@@ -1,7 +1,11 @@
+from django.utils import simplejson as json
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView,ListView,UpdateView
+from django.views.generic import DetailView, ListView, UpdateView
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from testproj.testappp.models import *
+from django.core import serializers
+
 
 class BioView(DetailView):
     model = BioModel
@@ -15,11 +19,24 @@ class RequestView(ListView):
     template_name = 'middleware_request.html'
     paginate_by = 10
 
+
 class EditBioView(UpdateView):
     model = BioModel
     template_name = 'edit_bio.html'
     success_url = '/'
     context_object_name = 'bio'
+
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        return super(EditBioView,self).dispatch(request, *args, **kwargs)
+        return super(EditBioView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        instanse = form.save()
+        return HttpResponse(
+            serializers.serialize(
+                "json",
+                [instanse, ]
+            ),
+            mimetype='text/json',
+            content_type='text/html; charset=utf-8'
+        )
