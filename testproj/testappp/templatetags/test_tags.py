28388febfe6d87bfcbdb98419ed0dcc8a2ee36b1
Lex <konstantin.lex@gmail.com>
2012-07-09 23:34:04 +0300

    PEP8

--- testproj/testappp/templatetags/test_tags.py
+++ testproj/testappp/templatetags/test_tags.py
@@ -7,7 +7,13 @@ from django.contrib.contenttypes.models import ContentType
 
 @register.simple_tag
 def edit_link(model):
-    content_type = ContentType.objects.filter(name__iexact = model.__class__.__name__)[0]
-    str = urlresolvers.reverse('admin:{0}_{1}_change'.format(content_type.app_label,content_type.model), args=(model.id,))
-    print str
+    content_type = ContentType.objects.filter(
+        name__iexact=model.__class__.__name__
+    )[0]
+    str = urlresolvers.reverse(
+        'admin:{0}_{1}_change'.format(
+            content_type.app_label,
+            content_type.model
+        ),
+        args=(model.id,))
     return str
